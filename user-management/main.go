2ef7d58f7bea37a2c3dd9e611fe83e16c732a5ec
akshay-misra-demo-apps <84061064+akshay-misra-demo-apps@users.noreply.github.com>
2023-09-27 20:37:22 +0530

    Chore user management (#1) (#2)
    * added user-management microservice
    
    * added Dockerfile and Makefile to build docker image and push new image to dockerhub.
    
    * added product-management microservice
    
    * port number fix for product management microservice
    
    * added more targets to make file
    
    * added docker compose file to bring all microservices up
    
    * update make up to pull fresh images always
    
    * updated readme with instructions to launch the application.

---
+++ user-management/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+
+	"github.com/akshay-misra-demo-apps/go-cart/user-management/controllers"
+	"github.com/akshay-misra-demo-apps/go-cart/user-management/middlewares"
+	"github.com/akshay-misra-demo-apps/go-cart/user-management/repositories"
+	"github.com/akshay-misra-demo-apps/go-cart/user-management/routes"
+	"github.com/akshay-misra-demo-apps/go-cart/user-management/services"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	// check if this is good from DI perspective
+	repositories.Init()
+}
+
+func App() {
+	server := gin.Default()
+	server.GET("/", home)
+
+	userRepository := repositories.Get()["user"]
+	userService := services.GetUserService(userRepository)
+	userController := controllers.GetUserController(userService)
+
+	// register routes without authentication
+	routes.NoAuthRoutes(server, userController)
+	// Authenticate all below routes
+	server.Use(middlewares.Authenticate())
+	// Authorize all below routes
+	server.Use(middlewares.Authorize())
+	routes.UserRoutes(server, userController)
+
+	if err := server.Run(":4000"); err != nil {
+		log.Fatalf("error while creating http server %v", err.Error())
+	}
+}
+
+// Consider using some DI like google.wire to manage dependencies.
+func main() {
+	App()
+}
+
+func home(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"response": "Welcome to go-cart user management service!",
+	})
+}
---
+++ user-management/models/models.go
@@ -0,0 +1,5 @@
+package models
+
+// Entity all models will be of this type
+type Entity struct {
+}
